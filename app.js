ad570ce6ad06619a287f7b38ecb064bd1980534f
Thiago Silva <thiagowfer@gmail.com>
2019-07-12 12:25:12 -0300

    Commit inicial + displaybooks funcionando

---
+++ app.js
@@ -0,0 +1,59 @@
+// Book class: Represent a book
+class Book{
+    constructor(title, author, isbn){
+        this.title = title;
+        this.author = author;
+        this.isbn = isbn;
+    }
+}
+
+// UI Class: Handle UI Tasks
+class UI {
+    static displayBooks(){
+        const StoredBooks = [
+            {
+                title: 'Book One',
+                author: 'John Doe',
+                isbn: '3434434'
+            },
+            {
+                title: 'Book Two',
+                author: 'Jane Doe',
+                isbn: '4545545'
+            },
+            {
+                title: 'Book Three',
+                author: 'Thiago F.',
+                isbn: '23882388'
+            }
+        ];
+
+    const books = StoredBooks;
+
+    books.forEach((book) => UI.addBookToList(book));
+   }
+
+   static addBookToList(book){
+       const list = document.querySelector('#book-list');
+       const row = document.createElement('tr');
+       
+       row.innerHTML = `
+       <td>${book.title}</td>
+       <td>${book.author}</td>
+       <td>${book.isbn}</td>
+       <td><a href="#" class="btn btn-danger btn-sm delete">X</a></td>
+       `;
+
+       list.appendChild(row);
+   }
+}
+
+// Store Class: Handles Storage
+
+// Event: Display Books
+document.addEventListener('DOMContentLoaded', UI.displayBooks);
+
+// Event: Add a book
+
+
+// Event: Remove a book
