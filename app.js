b0f25ecd5a93753381939972aa87514186053648
Thiago Silva <thiagowfer@gmail.com>
2019-07-12 15:46:17 -0300

    Validação

--- app.js
+++ app.js
@@ -47,6 +47,15 @@ class UI {
             el.parentElement.parentElement.remove();
         }
    }
+
+   static showAlert(message, className){
+        const div = document.createElement('div');
+        div.className = `alert alert-${className}`;
+        div.appendChild(document.createTextNode(message));
+        const container = document.querySelector('.container');
+        const form = document.querySelector('#book-form');
+        container.insertBefore(div, form);
+   }
    
    static clearFields(){
        document.querySelector('#title').value = '';
@@ -70,14 +79,18 @@ document.querySelector('#book-form').addEventListener('submit', (e) => {
     const author = document.querySelector('#author').value;
     const isbn = document.querySelector('#isbn').value;
 
-    //Instantiate book
-    const book = new Book(title, author, isbn);
+    if(title === '' || author === '' || isbn === ''){
+        UI.showAlert('Please fill in all the fields','danger');
+    } else {
+        //Instantiate book
+        const book = new Book(title, author, isbn);
 
-    //Add book to UI
-    UI.addBookToList(book);
+        //Add book to UI
+        UI.addBookToList(book);
 
-    //Clear fields
-    UI.clearFields();
+        //Clear fields
+        UI.clearFields();
+    }
 });
 
 // Event: Remove a book
